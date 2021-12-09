980fb7f24d92da6fc563a51b06aeca94be5098bd
Aries Ikel <71176569+40504@users.noreply.github.com>
2021-12-09 03:15:50 +0100

    update

--- _plugins/tost.rb
+++ _plugins/tost.rb
@@ -1,6 +1,39 @@
-File.open( thefile ).each do |line|
-  print line without the new line if line does not contain  /--+/
-  if line contains /--+/
-      print line with a new line
+# some_file = "text.txt"
+# if File.exist?(some_file)
+#     File.open(some_file).each do |line|
+#         if line.include?('a')
+#             puts "line: #{line}"
+#         end
+#     end
+# end
+
+
+Dir.foreach("../_data/services/") do |filename|
+  file_extension = File.extname(filename)
+  if file_extension == ".yml"
+    file_no_extension = File.basename(filename, ".yml")
+    file_title = file_no_extension.split('.').first
+    file_ser = file_no_extension.tr('.', '')
+    puts file_ser
+    File.open("../_services/#{file_no_extension}.html", "w") {
+|f| f.write(
+"---
+layout: main
+title: #{file_title}
+description: #{file_no_extension} Service page description
+permalink: /#{file_no_extension}/
+ser: #{file_ser}
+---
+<div class="+'"container"'+">{% include service.html %}</div>
+"
+)
+}
   end
+
+# File.open("../#{filename}.html", "w") {|f| f.write(filename) }
+  # Do work on the remaining files & directories
+  # filen =  File.basename(filename, ".*")
+  # puts filen
+  # puts "file name: #{filename}"
+  # print filename + "\n"
 end
