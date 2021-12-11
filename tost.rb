a01ef37d6c4e81d9ffdfdb5e50753a615e5d433d
Aries Ikel <71176569+40504@users.noreply.github.com>
2021-12-11 02:11:40 +0100

    update

--- tost.rb
+++ tost.rb
@@ -1,11 +1,11 @@
-Dir.foreach("../_data/services/") do |filename|
+Dir.foreach("_data/services/") do |filename|
   file_extension = File.extname(filename)
   if file_extension == ".yml"
     file_no_extension = File.basename(filename, ".yml")
     file_title = file_no_extension.split('.').first
     file_ser = file_no_extension.tr('.', '')
     puts file_ser
-    File.open("../_services/#{file_no_extension}.html", "w") {
+    File.open("_services/#{file_no_extension}.html", "w") {
 |f| f.write(
 "---
 layout: main
