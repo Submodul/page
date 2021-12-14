b6be9e036f12ae758c73910db07d076c36d9226a
Aries Ikel <71176569+40504@users.noreply.github.com>
2021-12-14 02:42:32 +0100

    Update svg-png.rb

--- svg-png.rb
+++ svg-png.rb
@@ -1,11 +1,15 @@
+require 'fileutils'
+
 Dir.foreach("assets/img/logo/") do |filename|
   file_extension = File.extname(filename)
   if file_extension == ".svg"
     file_no_extension = File.basename(filename, ".svg")
     file_title = file_no_extension.split(' ', 2).first
     system("cd ./assets/img/logo/ && mkdir #{file_title}") 
-    puts file_title
-    puts file_extension
+    # puts file_title
+    # puts file_extension
+    # system ("pwd")
+    # path = File.expand_path('../assets/img/logo/confirmo/', __FILE__)
     # svg -> png
     # system("cd ./assets/img/logo/ && rsvg-convert -w 1024 #{filename} > #{file_no_extension}.png ")
   end
